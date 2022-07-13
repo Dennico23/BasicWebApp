62875d7c3de88800391e770d9f29d7627da03d18
Dennis Quedzuweit <dennis.quedzuweit@web.de>
2022-07-13 13:45:53 +0200

    extend processor: plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,8 +14,6 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("752")) {
-            return "752";
         } else if (query.contains("what is your name")) {
             return "Dennis";
         } else if (query.contains("plus%20")) {
@@ -24,14 +22,8 @@ public class QueryProcessor {
         } else if (query.contains("plus")) {
             String[] reso = (String[]) Arrays.stream(query.split(" ")).toArray();
             return (Integer.parseInt(reso[2]) + Integer.parseInt(reso[4])) + "";
-        } else if (query.contains("what is 2 plus 4")) {
-            return "6";
-        } else if (query.contains("what is 14 plus 4")) {
-            return "18";
-        } else if (query.contains("838")) {
-            return "838";
         } else {
-            return "Dennis";
+            return query;
         }
     }
 }
